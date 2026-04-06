0d985580dbd34b00aaad81699223c581f143a51d
AaronGPM <aaronguillermo96@gmail.com>
2026-04-06 00:57:51 -0400

    fix: Solucion a localStorage convertido a sessionStorage

--- src/features/auth/components/RegisterForm.jsx
+++ src/features/auth/components/RegisterForm.jsx
@@ -82,8 +82,8 @@ export default function RegisterForm() {
       }
  
       // Guardar token y datos del usuario desde la respuesta de la API
-      localStorage.setItem("tokenPORT", result.token);
-      localStorage.setItem("usuario", JSON.stringify(result.data));
+      sessionStorage.setItem("tokenPORT", result.token);
+      sessionStorage.setItem("usuario", JSON.stringify(result.data));
  
       // Redirigir al login
       window.location.href = "/";
