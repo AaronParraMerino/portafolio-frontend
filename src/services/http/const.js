7c2e402648f91ba740944472a756a65929f9c7a5
AaronGPM <aaronguillermo96@gmail.com>
2026-06-22 20:08:46 -0400

    primer intento

--- src/services/http/const.js
+++ src/services/http/const.js
@@ -1,3 +1,3 @@
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://sparky.tis.cs.umss.edu.bo/api';
 
 export default BASE_URL;
